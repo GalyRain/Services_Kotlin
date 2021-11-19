4b67975c02b5a41a5ed5e956abb810b99cece6a0
ViaMay <vi-113@yandex.ru>
2021-11-20 03:14:55 +0500

    add notification

--- app/src/main/java/com/elienz_dragon/services_kotlin/MainActivity.kt
+++ app/src/main/java/com/elienz_dragon/services_kotlin/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.elienz_dragon.services_kotlin
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
 import com.elienz_dragon.services_kotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var id = 0
+
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
@@ -14,12 +23,35 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
         binding.simpleService.setOnClickListener {
-            startService(MyService.newIntent(this))
+            startService(MyService.newIntent(this, 25))
+        }
+        binding.foregroundService.setOnClickListener {
+            showNotification()
         }
     }
 
+    private fun showNotification() {
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle("Title")
+            .setContentText("Text")
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .build()
+        notificationManager.notify(id++, notification)
+    }
 
+    companion object {
 
+        private const val CHANNEL_ID = "channel_id"
+        private const val CHANNEL_NAME = "channel_name"
+    }
 }
