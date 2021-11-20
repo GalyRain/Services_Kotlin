37b4679d201e17a0ef8cb71753bf15a39183b08f
ViaMay <vi-113@yandex.ru>
2021-11-20 19:44:34 +0500

    job service and dequeueWork

--- app/src/main/java/com/elienz_dragon/services_kotlin/MyJobService.kt
+++ app/src/main/java/com/elienz_dragon/services_kotlin/MyJobService.kt
@@ -2,24 +2,37 @@ package com.elienz_dragon.services_kotlin
 
 import android.app.job.JobParameters
 import android.app.job.JobService
-import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.util.Log
 import kotlinx.coroutines.*
 
-class MyJobService: JobService() {
+class MyJobService : JobService() {
 
     private val coroutineScope = CoroutineScope(Dispatchers.Main)
 
+    override fun onCreate() {
+        super.onCreate()
+        log("onCreate")
+    }
 
     override fun onStartJob(params: JobParameters?): Boolean {
-        log("onStartJob")
-        coroutineScope.launch {
-            for (i in 0 until 100) {
-                delay(1000)
-                log("Timer $i")
+        log("onStartCommand")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            coroutineScope.launch {
+                var workItem = params?.dequeueWork()
+                while (workItem != null) {
+                    val page = workItem.intent.getIntExtra(PAGE, 0)
+
+                    for (i in 0 until 5) {
+                        delay(1000)
+                        log("Timer $i $page")
+                    }
+                    params?.completeWork(workItem)
+                    workItem = params?.dequeueWork()
+                }
+                jobFinished(params, false)
             }
-            jobFinished(params, true)
         }
         return true
     }
@@ -29,12 +42,6 @@ class MyJobService: JobService() {
         return true
     }
 
-    override fun onCreate() {
-        super.onCreate()
-        log("onCreate")
-    }
-
-
     override fun onDestroy() {
         super.onDestroy()
         coroutineScope.cancel()
@@ -42,11 +49,18 @@ class MyJobService: JobService() {
     }
 
     private fun log(message: String) {
-        Log.d("SERVICE_TAG", "MyService: $message")
+        Log.d("SERVICE_TAG", "MyJobService: $message")
     }
 
     companion object {
 
-        const val JOB_ID = 2
+        const val JOB_ID = 111
+        private const val PAGE = "page"
+
+        fun newIntent(page: Int): Intent {
+            return Intent().apply {
+                putExtra(PAGE, page)
+            }
+        }
     }
 }
